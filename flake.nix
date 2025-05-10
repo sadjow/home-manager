0350896e8e697a13d389f69bd0433dd0dc3af660
Sadjow Leão <sadjow@gmail.com>
2025-05-10 14:59:29 -0400

    update to use Apple Silicon

--- flake.nix
+++ flake.nix
@@ -16,7 +16,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Fast, Declarative, Reproducible, and Composable Developer Environments
     devenv = {
       url = "github:cachix/devenv/latest";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -25,32 +24,24 @@
 
   outputs = { self, nixpkgs, home-manager, darwin, devenv, ... }:
     let
-      # System types to support
-      supportedSystems = [ "x86_64-darwin" "aarch64-darwin" ];
+      supportedSystems = [ "aarch64-darwin" ];
 
-      # Helper function to generate an attrset for each supported system
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-      # Import nixpkgs with overlays for each supported system
       nixpkgsFor = forAllSystems (system: import nixpkgs {
         inherit system;
-        # Configure your nixpkgs instance
         config = {
           allowUnfree = true;
         };
-        # Add devenv overlay
         overlays = [
           devenv.overlays.default
         ];
       });
     in
     {
-      # Home manager configuration
       homeConfigurations."sadjow" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgsFor."x86_64-darwin"; # Intel Mac
-        # When switch to M4: Change to "aarch64-darwin"
+        pkgs = nixpkgsFor."aarch64-darwin";
 
-        # Specify home configuration modules
         modules = [
           ./home.nix
           # Make devenv available to home.nix
@@ -60,8 +51,7 @@
 
       # Optional: darwin configuration if you want to use nix-darwin too
       darwinConfigurations."sadjow-mac" = darwin.lib.darwinSystem {
-        system = "x86_64-darwin"; # Intel Mac
-        # When you switch to M4: Change to "aarch64-darwin"
+        system = "aarch64-darwin";
         modules = [
           # Your darwin configuration
           # Example: ./darwin-configuration.nix
