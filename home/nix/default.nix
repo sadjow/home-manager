e59b6a597d8d9e19049f5b69b19b332b018cc1e8
Sadjow Leão <sadjow@gmail.com>
2025-07-14 20:42:03 -0300

    update devenv

--- home/nix/default.nix
+++ home/nix/default.nix
@@ -9,8 +9,10 @@
     let
       caches = {
         "cache.nixos.org" = "6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
+        "cachix.cachix.org" = "KzcwKqacT4A3+Jn1fEL4GezqHSO3LKC79VpRj4QsdB8=";
         "devenv.cachix.org" = "w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
         "nix-community.cachix.org" = "mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+        "claude-code.cachix.org" = "m0kqRMQrW+sjthbPBDu0YvPRlEISbmmMk4dJRc1xf5E=";
       };
     in
     {
