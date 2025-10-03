c7b69c4f4d64b3893d1398f2a3e2bb3fcbafdf2b
Sadjow Leão <sadjow@gmail.com>
2025-10-03 18:22:20 -0300

    feat: add asdf global tool versions managed by home-manager

--- home.nix
+++ home.nix
@@ -73,6 +73,15 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    # Global asdf tool versions
+    ".tool-versions".text = ''
+      ruby 3.2.9
+      nodejs 22.17.1
+      elixir system
+      terraform system
+      yarn system
+    '';
+
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
